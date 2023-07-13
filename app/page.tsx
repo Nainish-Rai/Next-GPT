7f6f37beacdeb50949e89d6ef107d31cc171f66e
Nainish Rai <nainishraisjp@gmail.com>
2023-07-14 00:06:52 +0530

    fixd

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 import Link from "next/link";
 import Input from "./components/Input";
 
-async function getData(query: string) {
-  const res = await fetch(`/api/chat/${query}`);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData(query: string) {
+//   const res = await fetch(`/api/chat/${query}`);
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 export default async function Home() {
-  const data = await getData("act as teacher");
-  console.log(data);
-  return <main>{data}</main>;
+  // const data = await getData("act as teacher");
+  // console.log(data);
+  return <main>data</main>;
 }
